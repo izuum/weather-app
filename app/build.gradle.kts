717a495b6f591f651d9d3045fc9c4aa404cd0566
Roman <dstep.ds@gmail.com>
2023-09-09 02:41:00 +0300

    реализовал получение координат местоположения и зарпос погоды с сервера

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,20 @@ android {
   kotlinOptions {
     jvmTarget = "1.8"
   }
+  buildFeatures {
+    viewBinding = true
+  }
 }
 
 dependencies {
   implementation(libs.android.material)
+  implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+  implementation("androidx.fragment:fragment-ktx:1.6.1")
+  implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
---
+++ app/src/main/java/com/github/izuum/weatherapp/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.izuum.weatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<WeatherData>()
+}

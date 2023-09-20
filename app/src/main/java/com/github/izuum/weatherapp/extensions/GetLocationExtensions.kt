78458cecd1066e7e1352f6d71677c90df5c5ce73
Roman <dstep.ds@gmail.com>
2023-09-21 02:17:00 +0300

    сделал функции проверки разрешений на получение геолокации в виде расшрений

---
+++ app/src/main/java/com/github/izuum/weatherapp/extensions/GetLocationExtensions.kt
@@ -0,0 +1,39 @@
+package com.github.izuum.weatherapp.extensions
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.github.izuum.weatherapp.fragments.MainFragment
+
+fun Fragment.checkPermission(): Boolean {
+    return ActivityCompat.checkSelfPermission(
+        activity as AppCompatActivity,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                activity as AppCompatActivity,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun Fragment.isLocationEnabled(): Boolean {
+    val locationManager: LocationManager =
+        activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+}
+
+fun Fragment.requestPermission() {
+    ActivityCompat.requestPermissions(
+        activity as AppCompatActivity,
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ),
+        MainFragment.PERMISSION_REQUEST_ACCESS_LOCATION
+    )
+}
